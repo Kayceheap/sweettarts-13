80938c43cdac3cd27d112b4bfeb431b27fbba40c
Kayce Heap <kayce.heap@gmail.com>
2022-03-08 13:08:44 -0700

    Work on category controller routes

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 // import sequelize connection
 
 const app = express();
@@ -14,6 +14,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
 });
